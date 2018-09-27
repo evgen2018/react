2e494d4e96683516f3d2b0dc86d3fc034cc86322
evgen2018 <timofeev111@ukr.net>
2018-09-27 22:31:07 +0300

    sd

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { createStore } from 'redux';
+
+
+let store = createStore(counter);
+
+store.subscribe(()=>{console.log(store.getState())})
+function counter(state ={}, action) {
+    switch (action.type) {
+        case 'ADD':
+            return {...state, films:action.payload}
+        default:
+            return state
+    }
+}
 
 class App extends Component {
+    constructor(props){
+        super(props)
+
+        this.handleClick=this.handleClick.bind(this)
+    }
+
+
+    handleClick(){
+        axios.get(
+            'https://swapi.co/api/people/1/'
+        ).then((res)=>{
+            store.dispatch({type:"ADD" ,payload:res.data.films})
+        })
+    }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <h1>HELLOW WORLD FROM REACT</h1>
+      <div className="card" style={{width: `18rem`}}>
+          <div className="card-body">
+                  <button className="btn btn-success" onClick={this.handleClick}>Створити</button>
+            </div>
       </div>
+     </div>
     );
   }
 }
