25c7537f3aae223fe400912463f6ba73b2c78cd0
evgen2018 <timofeev111@ukr.net>
2018-09-28 01:38:28 +0300

    ds

--- src/App.js
+++ src/App.js
@@ -2,48 +2,83 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import { createStore } from 'redux';
-
-
-let store = createStore(counter);
-
-store.subscribe(()=>{console.log(store.getState())})
-function counter(state ={}, action) {
-    switch (action.type) {
-        case 'ADD':
-            return {...state, films:action.payload}
-        default:
-            return state
-    }
-}
+import { connect } from 'react-redux';
 
 class App extends Component {
     constructor(props){
         super(props)
+        this.state={
+            filmsCount: (  props.linksFilm.length ),
+            filmsOnPage:0,
+            nextFilmIndex:0,
+            noMore:false
+        }
 
         this.handleClick=this.handleClick.bind(this)
     }
 
-
-    handleClick(){
+    componentDidMount(){
         axios.get(
             'https://swapi.co/api/people/1/'
         ).then((res)=>{
-            store.dispatch({type:"ADD" ,payload:res.data.films})
+            this.props.addLinks(res.data.films)
         })
+    }
+
+    handleClick(){
+        const {addFilm, films, linksFilm} = this.props;
+        const {filmsOnPage, nextFilmIndex, filmsCount} =this.state;
+        if (filmsOnPage < linksFilm.length){
+        axios.get(linksFilm[nextFilmIndex]).then((res)=>{
+            addFilm(res.data);
+            this.setState({
+                nextFilmIndex: nextFilmIndex+1,
+                filmsOnPage: filmsOnPage+1
+            })
+        })} else {
+            this.setState({
+                noMore:true
+            })
+        }
     }
 
   render() {
     return (
       <div className="App">
-      <div className="card" style={{width: `18rem`}}>
-          <div className="card-body">
-                  <button className="btn btn-success" onClick={this.handleClick}>Створити</button>
-            </div>
-      </div>
+          <div className="frame">
+                <h2>Додати цей товар у список</h2>
+          <div  className="text-left new" > Cтворити новий список </div>
+                {this.state.noMore && <div  className="text-left noMore" > Більше немає </div> }
+              <div className="text-right create-block">
+                      <button className="btn pull-right create" onClick={this.handleClick}>Створити</button>
+              </div>
+              <div>
+                <ul>
+                    {this.props.films && this.props.films.map((film)=>{return(
+                      <li className="item">
+                          <div className="item__text item__title">{film.title}</div>
+                          <div className="item__text item__count">{film.starships.length}</div>
+                      </li>
+                    )})}
+                </ul>
+             <button className="btn btn-success add">Додати</button>
+              </div>
+          </div>
      </div>
     );
   }
 }
 
-export default App;
+export default connect((store)=>({
+    films     : store.films,
+    linksFilm : store.linksFilm,
+}),
+    dispatch=>({
+        addFilm: (film)=>{
+        dispatch({type:`ADD_FILM`, payload:film})},
+
+        addLinks: (linksFilm)=>{
+        dispatch({type:`ADD_LINKS`, payload:linksFilm})
+    }
+    })
+)(App);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider} from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+let store = createStore(counter);
+
+function counter(state ={films:[], linksFilm:[] }, action) {
+    switch (action.type) {
+        case 'ADD_FILM':
+            return {...state, films:[...state.films, action.payload]};
+        case 'ADD_LINKS':
+            return {...state, linksFilm:action.payload}
+default:
+    return state
+}
+}
+
+const render =()=>{
+    ReactDOM.render(
+    < Provider
+    store = {store} >
+        < App / >
+        < / Provider>,
+    document.getElementById('root')
+)};
+
+render();
+store.subscribe(()=>{render()})
